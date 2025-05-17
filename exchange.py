4f670d53016e8a8fbaebb3f853a0a5bf4c68d6d9
krak <notreallykrak@gmail.com>
2025-05-18 00:13:13 +0300

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -1,5 +1,6 @@
 import json
 import random
+import time
 
 DATA_FILE = "data.json"
 
@@ -9,9 +10,15 @@ def update_exchange_rates():
 
     for currency in data["exchange_rates"]:
         rate = data["exchange_rates"][currency]
-        change = rate * random.uniform(-0.25, 1.0)
+        change = rate * random.uniform(-0.25, 1.0)  # %25 düşüp %100 artabilir
         new_rate = max(1, int(rate + change))
         data["exchange_rates"][currency] = new_rate
 
     with open(DATA_FILE, "w") as f:
         json.dump(data, f, indent=4)
+    print("✅ Döviz kurları güncellendi!")
+
+if __name__ == "__main__":
+    while True:
+        update_exchange_rates()
+        time.sleep(120)  # 2 dakikada bir günceller
